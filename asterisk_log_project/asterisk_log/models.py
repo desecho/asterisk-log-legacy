b03674efc889d1881c54b3cc53fe309b9c0adfeb
Anton Samarchyan <desecho@gmail.com>
2014-11-02 13:40:58 +0000

    changes

--- asterisk_log_project/asterisk_log/models.py
+++ asterisk_log_project/asterisk_log/models.py
@@ -6,5 +6,5 @@ from django.db import models
 class Call(models.Model):
     calldate = models.DateTimeField('дата/время звонка')
     src = models.CharField('откуда', max_length=80)
-    dst = models.CharField('куда', max_length=255)
-    duration = models.PositiveIntegerField('длительность', unique=True)
+    dst = models.CharField('куда', max_length=80)
+    duration = models.PositiveIntegerField('длительность')
