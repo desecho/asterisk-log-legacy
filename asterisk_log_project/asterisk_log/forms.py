6720d9ea3a3f8499b1755466b14d9f853d2be80c
Anton Samarchyan <desecho@gmail.com>
2014-11-03 16:28:36 +0000

    changes

--- asterisk_log_project/asterisk_log/forms.py
+++ asterisk_log_project/asterisk_log/forms.py
@@ -6,14 +6,18 @@ from datetime import datetime
 # def convertToDatetime(date):
 #     return datetime.strptime(date, "%d.%m.%Y").date()
 
+regular_widget = forms.TextInput(attrs={'class': 'form-control'})
+
 
 class CallForm(forms.Form):
-    date_start = forms.DateField(label='Дата от', required=False)
-    date_end = forms.DateField(label='Дата по', required=False)
-    duration_from = forms.CharField(label='Длительность от', required=False)
-    duration_to = forms.CharField(label='Длительность по', required=False)
-    source = forms.CharField(label='Откуда', required=False)
-    destination = forms.CharField(label='Куда', required=False)
+    date = forms.DateField(label='Дата', required=False, widget=regular_widget)
+    date_start = forms.DateField(label='Дата от', required=False, widget=regular_widget)
+    date_end = forms.DateField(label='Дата по', required=False, widget=regular_widget)
+    duration_from = forms.CharField(label='Длительность от', required=False, widget=regular_widget)
+    duration_to = forms.CharField(label='Длительность по', required=False, widget=regular_widget)
+    phone = forms.CharField(label='Телефон', required=False, widget=regular_widget)
+    source = forms.CharField(label='Откуда', required=False, widget=regular_widget)
+    destination = forms.CharField(label='Куда', required=False, widget=regular_widget)
 
     # def clean_date_start(self):
     #     return convertToDatetime(self.cleaned_data['date_start'])
--- asterisk_log_project/asterisk_log/views.py
+++ asterisk_log_project/asterisk_log/views.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+from django.db.models.query_utils import Q
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
@@ -17,17 +19,22 @@ def home(request):
     form = CallForm(request.POST or None)
     calls = Call.objects.all()
     if form.is_valid():
+        if form.cleaned_data['date']:
+            calls = calls.filter(calldate__range=[form.cleaned_data['date'], form.cleaned_data['date'] + timedelta(days=1)])
         if form.cleaned_data['date_start']:
             calls = calls.filter(calldate__gte=form.cleaned_data['date_start'])
         if form.cleaned_data['date_end']:
-            calls = calls.filter(calldate__lte=form.cleaned_data['date_end'])
+            calls = calls.filter(calldate__lte=form.cleaned_data['date_end'] + timedelta(days=1))
         if form.cleaned_data['duration_from']:
             calls = calls.filter(duration__gte=form.cleaned_data['duration_from'])
         if form.cleaned_data['duration_to']:
             calls = calls.filter(duration__lte=form.cleaned_data['duration_to'])
+        if form.cleaned_data['phone']:
+            calls = calls.filter(Q(src=form.cleaned_data['phone']) | Q(dst=form.cleaned_data['phone']))
         if form.cleaned_data['source']:
             calls = calls.filter(src=form.cleaned_data['source'])
         if form.cleaned_data['destination']:
             calls = calls.filter(dst=form.cleaned_data['destination'])
-
+    print form.is_valid()
+    print form.errors
     return {'form': form, 'calls': calls}
